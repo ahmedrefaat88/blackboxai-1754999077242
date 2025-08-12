ec1a7d6c1b920dbb56f0fbfaaeb0fa20e79b3e7d
ahmedrefaat88 <140299396+ahmedrefaat88@users.noreply.github.com>
2025-08-12 15:46:08 +0400

    Add .next/static/chunks/src_app_hr_page_tsx_a993527f._.js 1754999167162

---
+++ .next/static/chunks/src_app_hr_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ebb356b4._.js",
+  "static/chunks/node_modules_70b4a030._.js"
+],
+    source: "dynamic"
+});
